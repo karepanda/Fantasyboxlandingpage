670b86dbb10eef0991fc4bf0c2aa5a422205cc2c
kare <karelysperezs@gmail.com>
2026-05-04 22:55:40 +0200

    changed header font and size added

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ export const Header: React.FC = () => {
   return (
     <header className=" top-0 z-40 bg-white/80 backdrop-blur-sm border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
-        <div className="text-xl font-semibold text-[var(--fb-text)]">
+        <div className="fb-heading text-3xl md:text-5xl">
           FantasyBox
         </div>
         <LanguageSwitcher />
